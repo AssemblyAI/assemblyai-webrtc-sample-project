4e3d73f31b7176a714d90dcd090d79ac7e6297c7
Dylan Fox <dylanbfox@gmail.com>
2018-07-09 11:06:56 -0700

    init

---
+++ js/EncoderWorker.js
@@ -0,0 +1,32 @@
+// manually rewritten from CoffeeScript output
+// (see dev-coffee branch for original source)
+importScripts('WavAudioEncoder.js');
+
+var buffers = undefined,
+    encoder = undefined;
+
+self.onmessage = function(event) {
+  var data = event.data;
+  switch (data.command) {
+    case 'start':
+      encoder = new WavAudioEncoder(data.sampleRate, data.numChannels);
+      buffers = data.process === 'separate' ? [] : undefined;
+      break;
+    case 'record':
+      if (buffers != null)
+        buffers.push(data.buffers);
+      else
+        encoder.encode(data.buffers);
+      break;
+    case 'finish':
+      if (buffers != null)
+        while (buffers.length > 0)
+          encoder.encode(buffers.shift());
+      self.postMessage({ blob: encoder.finish() });
+      encoder = undefined;
+      break;
+    case 'cancel':
+      encoder.cancel();
+      encoder = undefined;
+  }
+};
